8a995059c61ee9c1816f33cf06609e0ed351007c
Mallory Helms <mghelms101@gmail.com>
2018-08-20 21:08:21 -0400

    Excuse Dolly note added

--- Project1/src/dev1/HelloWorld.java
+++ Project1/src/dev1/HelloWorld.java
@@ -3,7 +3,7 @@ package dev1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!!");
+		System.out.println("Hello World!! Please excuse Dolly's spelling");
 
 	}
 
